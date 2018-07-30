1aa68d995dac0996270ac83e2e41b108566cdbf3
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2018-07-30 18:22:03 -0300

    Update - update projects render

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import Logo from '../components/Logo'
 import Container from '../components/Container'
 import Menu from '../components/Menu'
 
+import About from '../components/About'
 import Explanation from '../components/Explanation'
 import ExplanationDescription from '../components/ExplanationDescription'
 
@@ -25,6 +26,8 @@ import ProjectTitle from '../components/ProjectTitle'
 import ProjectDescription from '../components/ProjectDescription'
 import ProjectExplanation from '../components/ProjectExplanation'
 import ProjectExplanationWrapper from '../components/ProjectExplanationWrapper'
+import ProjectImageWithHoverWrapper from '../components/ProjectImageWithHoverWrapper'
+import ProjectHoverDescription from '../components/ProjectHoverDescription'
 
 import Footer from '../components/Footer'
 import FooterWrapper from '../components/FooterWrapper'
@@ -32,10 +35,6 @@ import FooterTitle from '../components/FooterTitle'
 import FooterSubTitle from '../components/FooterSubTitle'
 import FooterText from '../components/FooterText'
 import FooterLink from '../components/FooterLink'
-
-const About = styled(ExplanationDescription)`
-  padding: 15px 0;
-`
 
 export const query = graphql`
   query Home {
@@ -107,7 +106,10 @@ const Home = ({
           <ProjectItem key={title}>
             {idx % 2 === 0 ? (
               <ProjectImageWrapper to={`/project/${slug}`}>
-                <ProjectImage src={featuredImage} />
+                <ProjectImageWithHoverWrapper>
+                  <ProjectImage src={featuredImage} />
+                  <ProjectHoverDescription>{title}</ProjectHoverDescription>
+                </ProjectImageWithHoverWrapper>
               </ProjectImageWrapper>
             ) : (
               <ProjectExplanationWrapper>
@@ -127,7 +129,10 @@ const Home = ({
               </ProjectExplanationWrapper>
             ) : (
               <ProjectImageWrapper to={`/project/${slug}`}>
-                <ProjectImage src={featuredImage} />
+                <ProjectImageWithHoverWrapper>
+                  <ProjectImage src={featuredImage} />
+                  <ProjectHoverDescription>{title}</ProjectHoverDescription>
+                </ProjectImageWithHoverWrapper>
               </ProjectImageWrapper>
             )}
           </ProjectItem>
