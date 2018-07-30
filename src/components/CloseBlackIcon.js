26d5a8415be39368650174aefe82c993514a0eb7
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2018-07-30 10:33:05 -0300

    New - add component close black icon

---
+++ src/components/CloseBlackIcon.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import MenuIcon from './MenuIcon'
+import srcCloseBlackIcon from '../img/close-black-icon.png'
+
+export default () => <MenuIcon src={srcCloseBlackIcon} />
