234d1778dfe08d640d143bcd0bd0d5f206eedd4d
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2018-08-01 12:35:09 -0300

    Update - update project image wrapper breakpoints

--- src/components/ProjectImageWrapper.js
+++ src/components/ProjectImageWrapper.js
@@ -1,6 +1,11 @@
 import Link from 'gatsby-link'
 import styled from 'styled-components'
+import BreakPoints from './BreakPoints'
 
 export default styled(Link)`
   width: 50%;
+  ${BreakPoints({
+    width: ['100%', '50%', '50%'],
+    padding: ['0 !important', '0', '0'],
+  })};
 `
