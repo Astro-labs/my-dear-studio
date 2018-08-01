3021be534ba27f06f926d38881ffa138b34dbd7e
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2018-08-01 11:05:17 -0300

    Update - add media query for monitors

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components'
+import BreakPoints from './BreakPoints'
 
 export default styled.div`
   margin-left: 2rem;
   margin-right: 2rem;
+
+  @media screen and (min-width: 1600px) {
+    margin-left: 20rem;
+    margin-right: 20rem;
+  }
 `
