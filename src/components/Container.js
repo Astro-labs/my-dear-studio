323081b7da57e355719147dc96ec6405ebb04b1a
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2018-08-01 10:31:44 -0300

    Fix - fix container dimensions

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export default styled.div`
-  max-width: 68rem;
+  max-width: 86rem;
   margin-left: auto;
   margin-right: auto;
   padding-left: 1.2272727273rem;
