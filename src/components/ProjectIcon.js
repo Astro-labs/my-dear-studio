037723b5a6071dc88c9e9f292553e8ebfa556f0e
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2018-08-02 21:14:09 -0300

    Update - update project icon to use gatsby image

--- src/components/ProjectIcon.js
+++ src/components/ProjectIcon.js
@@ -1,11 +1,23 @@
+import React from 'react'
 import styled from 'styled-components'
+import Img from 'gatsby-image'
 import BreakPoints from './BreakPoints'
-// TODO: implement this gatsby-image lateron
-// import Img from 'gatsby-image'
 
-export default styled.img`
+const ProjectIcon = styled(Img)`
   ${BreakPoints({
     width: ['100%', '50%', '20%'],
     marginRight: ['0', '20px', '20px'],
   })};
 `
+
+export default ({ src }) => (
+  <ProjectIcon
+    sizes={src ? src.sizes : []}
+    style={{
+      height: '100%',
+      width: '100%',
+      minHeight: '300px',
+      minWidth: '300px',
+    }}
+  />
+)
