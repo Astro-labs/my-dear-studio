bfabbca2ced0128420700b8fcda423b7a66556cc
Guilherme Decampo <guilherme.decampo@gmail.com>
2018-08-09 11:20:11 -0300

    Update - remove 20rem margins

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,9 +3,4 @@ import styled from 'styled-components'
 export default styled.div`
   margin-left: 2rem;
   margin-right: 2rem;
-
-  @media screen and (min-width: 1600px) {
-    margin-left: 20rem;
-    margin-right: 20rem;
-  }
 `
