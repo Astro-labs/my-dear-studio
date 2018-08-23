28ca0b202524f75986ac04b2d71b2469a30adae5
Guilherme Decampo <guilherme.decampo@gmail.com>
2018-08-23 10:13:41 -0300

    Update: add responsiveness

--- src/components/ProjectHoverDescription.js
+++ src/components/ProjectHoverDescription.js
@@ -5,12 +5,10 @@ export default styled.p`
   top: 0;
   left: 0;
   padding: 20px 60px;
-  padding-right: 80px;
   color: #fff;
   word-break: break-word;
   font-size: 1.2rem;
   font-weight: 100;
-  text-align: justify;
   line-height: 2rem;
   text-overflow: ellipsis;
   font-family: 'Open Sans';
--- src/components/ProjectImagesWrapper.js
+++ src/components/ProjectImagesWrapper.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
+import BreakPoints from './BreakPoints'
 
 export default styled.div`
   display: flex;
+  ${BreakPoints({
+    flexDirection: ['column', 'row', 'row'],
+  })};
 `
